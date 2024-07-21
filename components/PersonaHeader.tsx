7882752bd7b5c62d01a73e9158564b767762ceca
Chamal Perera <chamallakshika09@gmail.com>
2024-07-21 10:55:56 +0530

    refactor quick edit modal

--- components/PersonaHeader.tsx
+++ components/PersonaHeader.tsx
@@ -1,26 +1,15 @@
 'use client';
 
 import { PERSONA_ICONS } from '@/assets/PersonaIcons';
-import React, { useState } from 'react';
-import QuickEditModal from './QuickEditModal';
+import React from 'react';
 import PersonaBadge from './PersonaBadge';
 import { usePersona } from '@/contexts/PersonaContext';
-import PencilIcon from '@/assets/icons/Pencil.icon';
+import QuickEditButton from './QuickEditButton';
 
 export default function PersonaHeader() {
   const { name, selectedAvatar, selectedColor } = usePersona();
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
   const foundAvatar = PERSONA_ICONS.find((icon) => icon.name === selectedAvatar);
-
-  const handleOpenModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-  };
 
   return (
     <div className="flex items-center p-4 rounded-lg gap-4 relative">
@@ -31,10 +20,7 @@ export default function PersonaHeader() {
         bgColor={selectedColor}
       />
       <h1 className="ml-4 text-2xl font-bold text-textPrimary">{name}</h1>
-      <button onClick={handleOpenModal}>
-        <PencilIcon />
-      </button>
-      {isModalOpen && <QuickEditModal onClose={handleCloseModal} />}
+      <QuickEditButton />
     </div>
   );
 }
--- components/QuickEditModal.tsx
+++ components/QuickEditModal.tsx
@@ -1,13 +1,12 @@
-'use client';
-
 import { useState } from 'react';
 import SvgIcon from './SvgIcon';
 import { PERSONA_ICONS } from '@/assets/PersonaIcons';
-import CloseIcon from '@/assets/icons/Close.icon';
 import PersonaBadge from './PersonaBadge';
 import CheckIcon from '@/assets/icons/Check.icon';
 import { colors } from '@/data/colors';
 import { usePersona } from '@/contexts/PersonaContext';
+import Modal from './Modal';
+import QuickEditOptionsSection from './QuickEditOptionsSection';
 
 interface QuickEditModalProps {
   onClose: () => void;
@@ -30,81 +29,59 @@ export default function QuickEditModal({ onClose }: QuickEditModalProps) {
   };
 
   return (
-    <>
-      <div className="fixed inset-0 bg-black bg-opacity-50 z-40" onClick={onClose}></div>
-      <div className="absolute top-full left-0 flex items-center justify-center z-50">
-        <div className="bg-white w-[420px] rounded-xl shadow-lg p-4 border border-[#E6E6E6] flex flex-col gap-4">
-          <div className="flex justify-between items-center mb-4">
-            <h2 className="text-2xl font-semibold flex-grow">Quick Edit</h2>
-            <button className="text-gray-500 hover:text-gray-700 text-xl" onClick={onClose}>
-              <CloseIcon />
-            </button>
-          </div>
-
-          <div className="flex gap-6 items-end">
-            <PersonaBadge
-              icon={foundAvatar?.icon}
-              height="h-[48px]"
-              divClasses="rounded-xl w-[80px] h-[80px] p-3"
-              bgColor={localColor}
+    <Modal onClose={onClose}>
+      <Modal.Header title="Quick Edit" onClose={onClose} />
+      <Modal.Content>
+        <div className="flex gap-6 items-end">
+          <PersonaBadge
+            icon={foundAvatar?.icon}
+            height="h-[48px]"
+            divClasses="rounded-xl w-[80px] h-[80px] p-3"
+            bgColor={localColor}
+          />
+          <div className="flex gap-2 items-start h-11">
+            <label htmlFor="name" className="font-semibold text-primary">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              value={localName}
+              onChange={(e) => setLocalName(e.target.value)}
+              className="h-full border-[#E6E6E6] border-[1.5px] rounded-md text-[#101010] p-3 flex-grow"
             />
-            <div className="flex gap-2 items-start h-11">
-              <label htmlFor="name" className="font-semibold text-primary">
-                Name
-              </label>
-              <input
-                type="text"
-                id="name"
-                value={localName}
-                onChange={(e) => setLocalName(e.target.value)}
-                className="h-full border-[#E6E6E6] border-[1.5px] rounded-md text-[#101010] p-3 flex-grow"
-              />
-            </div>
-          </div>
-
-          <label className="font-semibold text-textPrimary">Image</label>
-          <div className="flex flex-wrap gap-2 items-center justify-start">
-            {PERSONA_ICONS.map((avatar) => (
-              <button
-                key={avatar.name}
-                onClick={() => setLocalAvatar(avatar.name)}
-                className={`p-1 rounded-md w-10 h-10 flex items-center justify-center bg-[#F5F5F5] ${
-                  localAvatar === avatar.name ? 'border-2 border-black' : ''
-                }`}
-              >
-                <SvgIcon icon={avatar.icon} className="h-[25px] w-[auto]" />
-              </button>
-            ))}
-          </div>
-
-          <label className="font-semibold text-textPrimary">Color</label>
-          <div className="flex flex-wrap gap-2 items-center justify-start">
-            {colors.map((color) => (
-              <button
-                key={color}
-                onClick={() => setLocalColor(color)}
-                className={`p-1 rounded-md w-10 h-10 flex items-center justify-center ${
-                  localColor === color ? 'border-2 border-black' : ''
-                }`}
-                style={{ backgroundColor: color }}
-              >
-                {localColor === color && <CheckIcon />}
-              </button>
-            ))}
-          </div>
-
-          <hr className="border-[#E6E6E6]" />
-
-          <div className="flex justify-end gap-2">
-            <button onClick={onClose} className="text-textPrimary font-semibold px-6 py-3 rounded-md">
-              Cancel
-            </button>
-            <button onClick={handleSave} className="font-semibold bg-[#222ADD] text-white px-6 py-3 rounded-md">
-              Save
-            </button>
           </div>
         </div>
-      </div>
-    </>
+
+        <QuickEditOptionsSection
+          label="Image"
+          icons={PERSONA_ICONS}
+          selectedIcon={localAvatar}
+          setSelectedIcon={setLocalAvatar}
+          iconRenderer={(avatar) => <SvgIcon icon={avatar.icon} className="h-[25px] w-[auto]" />}
+          buttonClasses="bg-[#F5F5F5]"
+        />
+
+        <QuickEditOptionsSection
+          label="Color"
+          icons={colors}
+          selectedIcon={localColor}
+          setSelectedIcon={setLocalColor}
+          iconRenderer={(color) => (localColor === color ? <CheckIcon /> : null)}
+          iconStyle={(color) => ({ backgroundColor: color })}
+        />
+      </Modal.Content>
+
+      <hr className="border-[#E6E6E6]" />
+
+      <Modal.Actions>
+        <button onClick={onClose} className="text-textPrimary font-semibold px-6 py-3 rounded-md">
+          Cancel
+        </button>
+        <button onClick={handleSave} className="font-semibold bg-[#222ADD] text-white px-6 py-3 rounded-md">
+          Save
+        </button>
+      </Modal.Actions>
+    </Modal>
   );
 }
