729dc1e7a39efc2a1d2b677da210c6c404fe3550
Chamal Perera <chamallakshika09@gmail.com>
2024-07-24 20:56:24 +0530

    change the icons

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,9 @@
-import Image from 'next/image';
-
-import logo from '@/assets/images/logo.png';
+import LogoIcon from '@/assets/icons/Logo.icon';
 
 export default function Header() {
   return (
     <nav className="p-6 border-b border-[#E6E6E6] flex items-center justify-between">
-      <Image src={logo} alt="logo" height={32} width={183} />
+      <LogoIcon className="h-[32px] w-[auto]" />
       <div className="flex items-center justify-center gap-4">
         <span className="font-medium text-textPrimary">John Smith</span>
         <div className="w-10 h-10 rounded-full font-medium text-white bg-[#00256E] flex items-center justify-center">
